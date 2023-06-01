30c2faef49fabde98382a6edd67ef9d5f2e2fc4d
alwil <alexandra.wilczynska@gmail.com>
2023-06-01 15:43:09 +0200

    align code better

--- themes/intro_shiny/app.R
+++ themes/intro_shiny/app.R
@@ -6,29 +6,24 @@ library(plotly)
 ui <- fluidPage(
   sliderInput("point_size", "Change size of the points:",
               1, 10, 3),
-  selectInput("cut_filter", "Choose cut quality", 
+  selectInput("cut_filter", "Choose cut quality",
               choices = levels(diamonds$cut)),
   plotlyOutput("diamonds_plot"),
   tableOutput("diamonds_table")
 )
 
-server <- function(input, output){
-  
-  diamonds_new <- reactive( 
-    diamonds %>%
-    filter(cut == input$cut_filter)
-  )
+server <- function(input, output) {
+  diamonds_new <- reactive(diamonds %>%
+                             filter(cut == input$cut_filter))
   
   output$diamonds_plot <- renderPlotly({
-    
     p <- ggplot(data = diamonds_new()) +
       aes(x = carat,
-          y = price, 
+          y = price,
           color = color) +
       geom_point(size = input$point_size,
                  shape = 19,
-                 alpha = 0.5
-      )
+                 alpha = 0.5)
     
     ggplotly(p)
     
@@ -40,4 +35,3 @@ server <- function(input, output){
 }
 
 shinyApp(ui = ui , server = server)
-
